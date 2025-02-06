446f2d8c54a055917fb0fae81f49791fdfff45c9
erik van appeldoorn <erik@nforza.nl>
2025-02-06 10:30:11 +0100

    Added Northwind Application

---
+++ Northwind.Application/Application.cs
@@ -0,0 +1,9 @@
+namespace Northwind.Application;
+
+public class Application
+{
+    public void Run()
+    {
+        Console.WriteLine("Hello from App.cs");
+    }
+}
---
+++ Northwind.Application/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Northwind.Application;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = ConfigureServices();
+        var serviceProvider = services.BuildServiceProvider();
+        var application = serviceProvider.GetService<Application>();
+        application?.Run();
+    }
+
+    private static IServiceCollection ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddTransient<Application>();
+        
+        return services;
+    }
+}
